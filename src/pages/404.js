d8e6585b37ae239c59bf09da7790521eb1f6d939
Emmie Päivärinta <Emmie.Paivarinta@gmail.com>
2020-06-03 20:30:13 +0200

    Apply Material UI design to the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Layout from '../components/Layout';
+import {Paper, Typography, makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(2, 3, 3),
+  },
+  heading: {
+    marginBottom: theme.spacing(1),
+  },
+}));
 
 export default function NotFoundPage() {
+  const classes = useStyles();
+
   return (
     <Layout>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Paper className={classes.root}>
+        <Typography className={classes.heading} variant="h3" component="h1">
+          NOT FOUND
+        </Typography>
+        <Typography variant="body1">
+          You just hit a route that doesn&#39;t exist... the sadness.
+        </Typography>
+      </Paper>
     </Layout>
   );
 }
